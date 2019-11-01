822bf7ca34b6b5af067ec6884db9029e00cf56c8
Kaitian Xie <xkaitian@gmail.com>
2019-10-31 21:16:33 -0700

    Add 51 - 100

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,17 +3,24 @@ module.exports = {
   description: 'LeetCode solutions in Python, Java, and C++',
   themeConfig: {
     searchPlaceholder: 'Search...',
-    sidebar: [['/problems/1_50', '1 - 50'], ['/snippets/', 'Snippets']],
+    sidebar: [
+      ['/problems/1_50', '1 - 50'],
+      ['/problems/51_100', '51 - 100'],
+      ['/snippets/', 'Snippets'],
+    ],
   },
   markdown: {
     lineNumbers: true,
+    extendMarkdown: (md) => {
+      md.use(require('markdown-it-vuepress-code-snippet-enhanced'))
+    },
   },
   plugins: ['@vuepress/back-to-top', 'vuepress-plugin-nprogress'],
   configureWebpack: {
     resolve: {
       alias: {
-        '@alias': '../../'
-      }
-    }
-  }
+        '@alias': '../../',
+      },
+    },
+  },
 }
